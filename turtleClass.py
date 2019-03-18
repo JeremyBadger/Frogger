2388836dfd02f66aa7b623b5ab1db9a7bff2fe79
JeremyBadger <47323627+JeremyBadger@users.noreply.github.com>
2019-03-18 09:59:58 -0400

    Updated Turtle class

--- turtleClass.py
+++ turtleClass.py
@@ -1,4 +1,17 @@
 TURTLE = pygame.image.load('resources/turtle.png')
 LOG = pygame.image.load('resources/log.pngs')
 class WaterObject(pygame.sprite.sprite):
-    def __init__(self):
+    def __init__(self, posY, leftRight):
+        #things
+        self.leftRight = leftRight
+        self.y = posY
+        if self.leftRight == "LEFT":
+            self.x = 400
+        else:
+            self.x == 0
+        self.rect = pygame.Rect(self.x, self.y, (25,25))
+    def move(self, self.leftRight):
+        if self.leftRight == "LEFT":
+            self.x -= 25
+        else:
+            self.x += 25
