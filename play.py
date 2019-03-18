151c65c34bb2568ca1f2dba9f595f54674f20422
ashenley <33043435+ashenley@users.noreply.github.com>
2019-03-18 10:00:34 -0400

    comments
    added comments for where each piece of code is supposed to go

---
+++ play.py
@@ -0,0 +1,20 @@
+#init and whatnot idk
+
+#define everything whoever wants to do that
+
+#add_cars
+
+#update_cars
+
+#update_frog
+
+#add_turtles
+
+#add_logs
+
+#scorebox
+
+#is_collision
+
+#while True:
+    #blah blah blah etc
