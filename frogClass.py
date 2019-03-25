6976d728e3f34263190d43375a714ac85bb4ea21
JeremyBadger <47323627+JeremyBadger@users.noreply.github.com>
2019-03-25 14:09:00 -0400

    Update frogClass.py

--- frogClass.py
+++ frogClass.py
@@ -6,6 +6,7 @@ class frog(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = frog.png
+        self.imgage = pygame.transform.scale(frog.png, (25,25))
         self.rect = pygame.Rect(25,25,25,25)
         points = 0
         self.x = 275
