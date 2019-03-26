befe2c6d86c66ea124ebae2e1a5e59065db474f2
JeremyBadger <47323627+JeremyBadger@users.noreply.github.com>
2019-03-26 15:16:26 -0400

    more errors regarding frog self image

--- frogClass.py
+++ frogClass.py
@@ -1,12 +1,12 @@
 import pygame
 from pygame.locals import *
-
+FROG = pygame.image.load('resources/frog.png')
 
 class frog(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = 'frog.png'
-        self.imgage = pygame.transform.scale('frog.png', (25,25))
+        self.image = FROG
+        self.imgage = pygame.transform.scale(FROG, (25,25))
         self.rect = pygame.Rect(25,25,25,25)
         points = 0
         self.x = 275
