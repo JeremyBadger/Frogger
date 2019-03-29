6cbe246f4069f778b36e3ed774f5fbca09feb47c
ashenley <33043435+ashenley@users.noreply.github.com>
2019-03-29 10:00:54 -0400

    fixed issues with update_turtles() maybe

--- play.py
+++ play.py
@@ -41,9 +41,9 @@ def update_cars():
         car1.move()
         car1.update()
         DISPLAYSURF.blit(car1.image, car1.rect)
-    if car.rect.x <= 0 or car.rect.x >= 400:
+    if car.rect.x < 0 or car.rect.x > 400:
         car1.kill()
-    elif car.rect.y <= 0 or car.rect.y >= 300:
+    elif car.rect.y < 0 or car.rect.y > 300:
         car1.kill()
 
 #def update_frog():
@@ -61,9 +61,7 @@ def update_turtles():
     for turtle1 in waterObjects:
         turtle1.update()
         DISPLAYSURF.blit(turtle1.image, turtle1.rect)
-    if turtle.rect.x <= 0 or turtle.rect.x >= 400:
-        turtle1.kill()
-    elif turtle.rect.y <= 0 or turtle.rect.y >= 300:
+    if water.y < 0 or water.y > 300:
         turtle1.kill()
 
 #def update_logs():
