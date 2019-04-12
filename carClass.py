43f11b49e9077f11a21b04e24a7ee608f90c6d0f
Myles Evans <mevans2020@ravenscroft.org>
2019-04-12 11:45:42 -0400

    Update carClass.py
    Added comments for car class.

--- carClass.py
+++ carClass.py
@@ -1,12 +1,12 @@
-import pygame, sys, time, random
+import pygame, sys, time, random #importing librarys
 from pygame.locals import *
 
-CAR= pygame.image.load('resources/car.png')
+CAR= pygame.image.load('resources/car.png') #calling the same image for both the car variables
 CAR2= pygame.image.load('resources/car.png')
 
 
 class cars(pygame.sprite.Sprite):
-    def __init__(self, ground, leftRight, speed):
+    def __init__(self, ground, leftRight, speed): #the init function setting all the variables
         super().__init__()
         self.x=0
         self.y=ground
@@ -14,13 +14,13 @@ class cars(pygame.sprite.Sprite):
         self.image=CAR
         self.speed = speed
         self.type = "CAR"
-        if self.leftRight == "LEFT":
+        if self.leftRight == "LEFT": #setting up all the cars going to the left/right
             self.x = 400
         else:
             self.x = 0
-        self.rect=pygame.Rect(self.x,self.y,10,10)
+        self.rect=pygame.Rect(self.x,self.y,10,10) #car hitboxes
 
-    def update(self):
+    def update(self):    #determining how fast each car moves and when they respawn back on the screen
         if self.leftRight == "LEFT":
             if self.x>0:
                 self.rect.x -= self.speed
